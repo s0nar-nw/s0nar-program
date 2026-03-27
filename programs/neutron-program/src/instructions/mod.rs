64046fb97ad702dbaba9f5a9842c65bd13b552db
JKrishnaD <jayak5063@gmail.com>
2026-03-27 16:53:02 +0530

    feat: add crank aggregation ix

--- programs/neutron-program/src/instructions/mod.rs
+++ programs/neutron-program/src/instructions/mod.rs
@@ -1,7 +1,9 @@
+pub mod crank_aggregation;
 pub mod initialize;
 pub mod register_observer;
 pub mod submit_attestation;
 
+pub use crank_aggregation::*;
 pub use initialize::*;
 pub use register_observer::*;
 pub use submit_attestation::*;
